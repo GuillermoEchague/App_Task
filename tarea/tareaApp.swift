545bd931dd80e8eb9a77e34d1eabfef30940f623
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2024-12-09 19:06:03 -0300

    Initial Commit

---
+++ tarea/tareaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tareaApp.swift
+//  tarea
+//
+//  Created by Guillermo Echague on 09-12-24.
+//
+
+import SwiftUI
+
+@main
+struct tareaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
